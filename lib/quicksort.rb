fe8462e978d3e32f9ece11f23ab3e9524f00d43c
Christine Seeman <christine.seeman@wpengine.com>
2023-03-24 09:36:14 -0500

    Adding private methods to quicksort
    I wanted to clarify the contract with this quicksort. You only want to
    use the quick_sort method, the other two for partitioning and swaping
    are used internally. To simplify it, just changing them to private
    methods.

--- lib/quicksort.rb
+++ lib/quicksort.rb
@@ -1,42 +1,44 @@
 # frozen_string_literal: true
 
-#
 # Code orginally from https://dev.to/mwong068/quick-sort-in-ruby-2302
 # All credit to the original author @mwong068, https://github.com/mwong068
 #
 # This is monkeypatching Array, so just use an array!
 # irb -r ./quicksort
 # arr = [9,7,8,3,6,4,1,2,5]
-# arr.quick_sort 
+# arr.quick_sort
 # >> [1, 2, 3, 4, 5]
 #
 # @return Array in sorted order
 class Array
-  def quick_sort(from_index = 0, to_index = length - 1)
-     if from_index < to_index
-       pivot_index = partition_and_get_pivot_index(from_index, to_index)
-       quick_sort(from_index, pivot_index - 1)
-       quick_sort(pivot_index + 1, to_index)
-     end
-     return self
-   end
+  def quick_sort(start_index = 0, end_index = length - 1)
+    # How you determine the smallest case
+    if start_index < end_index
+      pivot_index = partition_and_get_pivot_index(start_index, end_index)
+      quick_sort(start_index, pivot_index - 1)
+      quick_sort(pivot_index + 1, end_index)
+    end
+    self # Return the smallest case, no more recursion needed
+  end
 
-   def partition_and_get_pivot_index(from_index, to_index)
-     pivot_value = self[to_index]
-     pointer_a_index = pointer_b_index = from_index
+  private
 
-     while pointer_a_index < to_index
-       if self[pointer_a_index] <= pivot_value
-         swap_values(pointer_a_index, pointer_b_index)
-         pointer_b_index += 1
-       end
-       pointer_a_index += 1
-     end
-     swap_values(pointer_b_index, to_index)
-     return pointer_b_index
-   end
+  def partition_and_get_pivot_index(from_index, to_index)
+    pivot_value = self[to_index]
+    pointer_a_index = pointer_b_index = from_index
 
-   def swap_values(index_a, index_b)
-     self[index_a], self[index_b] = self[index_b], self[index_a]
-   end
+    while pointer_a_index < to_index
+      if self[pointer_a_index] <= pivot_value
+        swap_values(pointer_a_index, pointer_b_index)
+        pointer_b_index += 1
+      end
+      pointer_a_index += 1
+    end
+    swap_values(pointer_b_index, to_index)
+    pointer_b_index
+  end
+
+  def swap_values(index_a, index_b)
+    self[index_a], self[index_b] = self[index_b], self[index_a]
+  end
 end
